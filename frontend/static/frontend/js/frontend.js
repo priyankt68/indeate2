aaccda1044fd8cc9667280671dd55bce16b97132
Priyank Trivedi <priyankt68@gmail.com>
2016-10-15 21:20:41 +0530

    1. Adding ids for button elements. 2. Adding javascript for jquery functions. To add more related to ajax later.

---
+++ frontend/static/frontend/js/frontend.js
@@ -0,0 +1,20 @@
+/**
+ * Created by priyanktrivedi on 15/10/16.
+ */
+
+console.log("I'm called.");
+
+
+$("#btn_1_big").click(function() {
+  alert( "First big button is clicked." );
+});
+
+$("#btn_2").click(function() {
+  alert( "Button with id btn_2 is clicked." );
+});
+
+$("#btn_3").click(function() {
+  alert( "Button with id btn_3 is clicked." );
+});
+
+// Making an ajax call to a view in python and then loading an html.
